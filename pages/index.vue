46271e6900ebde78c32b274df58c7083854aa2e9
Kevin Rodrigues <kevin.rodrigues@just-eat.com>
2020-04-10 23:47:35 +0100

    Covid-19 - Added config.

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
       <VirusIcon />
       <h1 class="title">
         <WorldIcon class="world" />
-        Covid-19 Visualizer
+        Covid-19 Is Real
       </h1>
       <div class="container is-fullhd">
         <div class="visualizer-info">
